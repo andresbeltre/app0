214d7b7387d0c0c3ec1631d010206b520cc08e15
root <mcrossdev@gmail.com>
2019-09-28 23:41:14 -0400

    fixing client?

--- services/passport.js
+++ services/passport.js
@@ -20,12 +20,7 @@ passport.use(new GoogleStrategy({
     callbackURL: "http://localhost:5000/auth/google/callback"
   },
   (accessToken, refreshToken, profile, done) => {
-      //console.log('accessToken',accessToken);
-      //console.log('refreshToken', refreshToken);
       console.log('profile', profile);
-      /*User.findOrCreate({ googleID: profile.id, email: profile.emails[0].value, firstName: profile.name.givenName, lastName: profile.name.familyName }, function (err, user) {
-        return done(err, user);
-      });*/
       User.findOne({googleID: profile.id}).then(existingUser => {
         if (existingUser) {
           done(null, existingUser);
@@ -33,28 +28,5 @@ passport.use(new GoogleStrategy({
           new User({ googleID: profile.id }).save().then(user => done(null,user));
         }
       });
-      /*User.findOne({
-        googleID: profile.id 
-    }, function(err, user) {
-        if (err) {
-          console.log(err);
-            return done(err);
-        }
-        if (!user) {
-            user = new User({
-                googleID: profile.id,
-                firstName: profile.name.givenName,
-                lastName: profile.name.familyName,
-                email: profile.emails[0].value
-            });
-            user.save(function(err) {
-                if (err) console.log(err);
-                return done(err, user);
-            });
-        } else {
-            console.log(err);
-            return done(err, user);
-        }
-    });*/
   }
 ));
