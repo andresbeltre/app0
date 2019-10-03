19ed512caf63ebc3ad2ac9a770ea1f402b9018aa
andresbeltre <mr.andres01@gmail.com>
2019-10-02 22:38:27 -0400

    Refactored passport.js

--- services/passport.js
+++ services/passport.js
@@ -1,10 +1,10 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
-const mongoose = require('mongoose');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
+const mongoose = require("mongoose");
 
 mongoose.connect(keys.mongoURI);
-const User = mongoose.model('users');
+const User = mongoose.model("users");
 
 passport.serializeUser(function(user, done) {
   done(null, user._id);
@@ -14,19 +14,27 @@ passport.deserializeUser(function(userId, done) {
   User.findById(userId, (err, user) => done(err, user));
 });
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: "http://localhost:5000/auth/google/callback"
-  },
-  (accessToken, refreshToken, profile, done) => {
-      console.log('profile', profile);
-      User.findOne({googleID: profile.id}).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ googleID: profile.id, firstName: profile.name.givenName, lastName: profile.name.familyName, email: profile.emails[0].value}).save().then(user => done(null,user));
-        }
-      });
-  }
-));
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "http://localhost:5000/auth/google/callback"
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      console.log("profile", profile);
+      const existingUser = await User.findOne({ googleID: profile.id });
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        const user = await new User({
+          googleID: profile.id,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          email: profile.emails[0].value
+        }).save();
+        done(null, user);
+      }
+    }
+  )
+);
