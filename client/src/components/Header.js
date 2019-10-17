e55c5615d0dedf28b72e8c3e8617da6ba39e1188
andresbeltre <mr.andres01@gmail.com>
2019-10-16 22:43:41 -0400

    Header is almost done
    almost there boiii

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,29 +7,29 @@ class Header extends Component {
       case null:
         return "Still Deciding...";
       case false:
-        return "LOGGED IN";
+        return <a href="/auth/google/">Log In</a>;
       default:
-        return "GET THE FUCK OUT OF MY FACE YOU ARE NOT PART OF THE VIP CLUB. stay logged out.";
+        return <a href="/api/logout">Log Out</a>;
     }
   }
 
   render() {
+    console.log(this.props);
     return (
-      <div className="row">
-        <div className="col-md-12">
-          <nav className="navbar navbar-light bg-light">
-            <a className="navbar-brand" href="/">
-              <img
-                src={process.env.PUBLIC_URL + '/clippy.jpeg'}
-                width="30"
-                height="30"
-                class="d-inline-block align-top"
-                alt=""
-              />
-              Bootstrap
-            </a>
-          </nav>
-        </div>
+      <div className="col-md-12">
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand" href="/">
+            <img
+              src={process.env.PUBLIC_URL + "/clippy.jpeg"}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt=""
+            />
+            Bootstrap
+          </a>
+          <span>{this.renderContent()}</span>
+        </nav>
       </div>
     );
   }
