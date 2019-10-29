8d60a17218bbfed3c6b405da1d9b4481d9d6980f
andresbeltre <mr.andres01@gmail.com>
2019-10-28 23:42:45 -0400

    Updates to header
    added nav items, and condition to determine if user is logged in or not

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
   renderContent() {
@@ -12,22 +13,72 @@ class Header extends Component {
         return <a href="/api/logout">Log Out</a>;
     }
   }
-  
+
   render() {
     return (
       <div className="col-md-12">
-        <nav className="navbar navbar-light bg-light">
-          <a className="navbar-brand" href="/">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <Link
+            className="navbar-brand"
+            to={this.props.auth ? "/Dashboard" : "/"}
+            data-toggle="tooltip"
+            title="Dashboard"
+          >
             <img
               src={process.env.PUBLIC_URL + "/clippy.jpeg"}
               width="30"
               height="30"
               className="d-inline-block align-top"
-              alt=""
+              alt="app0"
             />
-            Bootstrap
-          </a>
-          <span>{this.renderContent()}</span>
+            app0
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-between"
+            id="navbarNav"
+          >
+            {this.props.auth ? (
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link" href="/">
+                    Friends
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/">
+                    HomeBase
+                  </a>
+                </li>
+              </ul>
+            ) : (
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link" href="/">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/">
+                    Feedback
+                  </a>
+                </li>
+              </ul>
+            )}
+            <ul className="navbar-nav mr-only">
+              <li className="nav-item">{this.renderContent()}</li>
+            </ul>
+          </div>
         </nav>
       </div>
     );
