b870f1f671fb05a9f8fab5fac171dd38aaebd064
andresbeltre <mr.andres01@gmail.com>
2019-11-01 18:02:13 -0400

    Small Edits

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class Dashboard extends Component {
   render() {
-    return(
-      <div id="dashboard" className="col-md-12">
-        <div className="row">
-        <div className="col-md-2">
-          Hello, world!
-        </div>
+    return (
+      <div id="dashboard" className="row">
+        <div className="col-md-2">Hello, world!</div>
         <div className="col-md-10 mainDashboardContent">
           <h1>{this.props.user.firstName}</h1>
-        </div>
         </div>
       </div>
     );
   }
-};
+}
 
 function mapStateToProps({ user }) {
   return { user };
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 
 class Landing extends Component {
   render() {
-    return <div>Landing</div>;
+    return (
+      <div id="landing" className="row">
+        <div className="col-md-12 col-lg-12">Landing Component</div>
+      </div>
+    );
   }
 }
 
